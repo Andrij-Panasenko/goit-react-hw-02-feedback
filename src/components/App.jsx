f24e730dfb7900bbb132096a6802614e5b48fcb3
a.panasenko <andrii.panasenko295@gmail.com>
2023-11-04 21:50:41 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,56 @@
-import { Component } from "react";
-import { Section } from "./Section";
-import { Statistics } from "./Statistics";
-import { Notification } from "./Notification";
-
+import { Component } from 'react';
+import { Section } from './Section';
+import { Statistics } from './Statistics';
+import { Notification } from './Notification';
 
 export class App extends Component {
-    state = {
-      good: 0,
-      neutral: 0,
-      bad: 0
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
   };
-  
+
   updateGood = () => {
-      this.setState(prevState => {
-         return {
-            good: prevState.good + 1,
-         }
-      })
-   }
+    this.setState(prevState => {
+      return {
+        good: prevState.good + 1,
+      };
+    });
+  };
 
-   updateNeutral = () => {
-      this.setState(prevState => {
-         return {
-            neutral: prevState.neutral + 1,
-         }
-      })
-    }
-   
-   updateBad = () => {
-      this.setState(prevState => {
-         return {
-            bad: prevState.bad + 1,
-         }
-      })
-   }
-   
-   countTotalFeedback = () => {
-      const { good, neutral, bad } = this.state;
-      return good + neutral + bad; 
-   }
+  updateNeutral = () => {
+    this.setState(prevState => {
+      return {
+        neutral: prevState.neutral + 1,
+      };
+    });
+  };
 
-   countPositiveFeedbackPercentage = () => {
-      const { good } = this.state;
-      const total = this.countTotalFeedback();
-      const precentage = good / total * 100;
+  updateBad = () => {
+    this.setState(prevState => {
+      return {
+        bad: prevState.bad + 1,
+      };
+    });
+  };
 
-      return Number(precentage.toFixed(0))
-   }
-  
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    const precentage = (good / total) * 100;
+
+    return Number(precentage.toFixed(0));
+  };
+
   render() {
-    const {good, neutral, bad } = this.state;
+    const { good, neutral, bad } = this.state;
     const totalFeedback = this.countTotalFeedback();
-    const precentagePositiveFeedback = this.countPositiveFeedbackPercentage()
+    const precentagePositiveFeedback = this.countPositiveFeedbackPercentage();
     return (
       <div>
         <Section
@@ -63,18 +62,20 @@ export class App extends Component {
           updateBad={this.updateBad}
           updateNeutral={this.updateNeutral}
         />
-        {totalFeedback ? <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          precentage={precentagePositiveFeedback}
-          total={totalFeedback}
-          value={this.state}
-          title={"Statistics"}
-          /> : <Notification message="There is no feedback"/>}
+        {totalFeedback ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            precentage={precentagePositiveFeedback}
+            total={totalFeedback}
+            value={this.state}
+            title={'Statistics'}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
       </div>
-    )
+    );
   }
-};
-
-
+}
